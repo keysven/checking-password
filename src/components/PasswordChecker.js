f59e58972f40e486755778ca0b11a0ede91385b1
keysven <keysven0@gmail.com>
2024-02-07 08:02:01 -0800

    checking password

--- src/components/PasswordChecker.js
+++ src/components/PasswordChecker.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PasswordStrength from "./PasswordStrength";
 
 const PasswordChecker = () => {
+  const [ showPassword , setShowPassword ] = useState(false);
+  const [ password, setPassword ] = useState('');
+  const [ passwordStrength, setPasswordStrength ] = useState(0);
+  const setAndCheckPassword = (password) => {
+    setPassword(password);
+    const hasUppercasae = /[A-Z]/.test(password);
+    const hasLowercasae = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[$#&!@_]/.test(password);
+    const isLengthValid = password.length >= 8;
+
+    const conditionsMet = [hasUppercasae, hasLowercasae, hasNumber, hasSpecialChar, isLengthValid];
+    console.log(hasUppercasae, hasNumber, hasLowercasae, hasSpecialChar, isLengthValid)
+    const metCount = conditionsMet.filter((a) => a=== true).length;
+    console.log('metCount', metCount)
+    
+    if (password.length) {
+      if (metCount > 4) { 
+        setPasswordStrength(2);
+      } else if ( metCount > 2 ) {
+        setPasswordStrength(1);
+      } else {
+        setPasswordStrength(0);
+      }
+    }
+    console.log('PasswordStrength', passwordStrength)
+  }
+
+  useEffect(() => {
+    setPasswordStrength(4)
+  }, [])
   return (
     <div className="layout-column align-items-center justify-content-center py-40 mt-100">
       <div className="card w-50 px-75 py-30">
@@ -9,21 +40,24 @@ const PasswordChecker = () => {
           <h2>Enter Your Password</h2>
           <div className="layout-column mb-10">
             <input
-              type={"password"}
+              type={showPassword ? "text" : "password"}
               id="name"
               placeholder="Enter Password"
               data-testid="passwordInput"
-              value={"password"}
+              value={password}
+              onChange={(e) => setAndCheckPassword(e.target.value) }
             />
           </div>
         </form>
         <div className="py-10" data-testid="buttonDiv">
-          <button>Show Password</button>
-          <button>Clear Password</button>
+        <button onClick={() => setShowPassword((previousState) => !previousState)}>
+          {showPassword ? "Hide Password" : "Show Password"}
+        </button>
+          <button onClick={() => {setPassword("")}}>Clear Password</button>
         </div>
       </div>
       <div className="w-50 py-20">
-        <PasswordStrength />
+        <PasswordStrength passwordStrength = {passwordStrength} />
       </div>
     </div>
   );
--- src/components/PasswordStrength.js
+++ src/components/PasswordStrength.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-const PasswordStrength = () => {
+const PasswordStrength = ({passwordStrength}) => {
+  console.log('Password Strength :', passwordStrength)
+  const colors = ['green','red','yellow']
+  const colorPerLengh = () => {
+    if (passwordStrength === 4) {return colors[0]}
+    else if(passwordStrength === 2) {return colors[0]}
+    else if(passwordStrength === 1) {return colors[2]}
+    else return colors[1] 
+  }
+  console.log('colors')
   return (
     <div
       className="px-5 py-5"
       style={{
-        backgroundColor: "green",
+        backgroundColor: colorPerLengh(),
       }}
       data-testid="passwordStrengthDiv"
     >
